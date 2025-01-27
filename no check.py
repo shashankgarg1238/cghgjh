150a556074a86db8e3b46936fc03494db7abe82b
shashankgarg1238 <gargshashank1238@gmail.com>
2025-01-27 17:21:43 +0530

    Add files via upload

---
+++ no check.py
@@ -0,0 +1,7 @@
+num = float(input("Enter the number: "))
+if num > 0 :
+    print("no. is positive")
+elif num==0 :
+    print("no is zero")
+else :
+    print("no is negative")
---
+++ si.py
@@ -0,0 +1,5 @@
+princi = float(input("enter the principle"))
+rate = float(input("enter the rate"))
+time = float(input("enter the time"))
+si=((princi)*(rate)*(time))/100
+print(f"the simple interest is: { si }")
